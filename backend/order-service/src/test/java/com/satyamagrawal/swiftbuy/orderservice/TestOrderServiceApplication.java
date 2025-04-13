d3f6b7fc2b5a21b251de8f5fc24655195ab73182
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2025-04-14 01:43:41 +0530

    Initialize the backend services

---
+++ backend/order-service/src/test/java/com/satyamagrawal/swiftbuy/orderservice/TestOrderServiceApplication.java
@@ -0,0 +1,11 @@
+package com.satyamagrawal.swiftbuy.orderservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(OrderServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
